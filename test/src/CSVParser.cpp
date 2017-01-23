6baa3884086d0024ddd304464709f70746b29361
Cristian Szabo <cristian.szabo@outlook.com>
2017-01-23 19:25:07 +0000

    Add CSVParser tests.

--- test/src/CSVParser.cpp
+++ test/src/CSVParser.cpp
@@ -6,10 +6,20 @@ using namespace bandit;
 
 go_bandit([]() {
     describe("CSVParser Test", []() {
-        CSVParser<1> csv(R"(column1 ,column2\ncell1,cell2\n)");
+        it("should trim tab character from header column", [&]() {
+            CSVParser<2> csv("column1\t,column2\t\n");
+            csv.readHeader("column1", "column2");
+        });
 
-        it("should trim white spaces from column and header", [&]() {
-            AssertThat(3, Equals(3));
+        it("should trim tab character from row cell", [&]() {
+            CSVParser<2> csv("column1,column2\ncell1\t,cell2\t\n");
+            csv.readHeader("column1", "column2");
+
+            std::string cell1, cell2;
+            csv.readRow(cell1, cell2);
+
+            AssertThat(cell1, Equals("cell1"));
+            AssertThat(cell2, Equals("cell2"));
         });
     });
 });
